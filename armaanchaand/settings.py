0395a920a4a3a2477044786a78f9198b8bc30ea4
ArmaanChaand <armaan.chaand17@gmail.com>
2023-04-26 14:12:54 +0530

    integrated

--- armaanchaand/settings.py
+++ armaanchaand/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'armaanchaand.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'portfolio-react-vite/dist'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,8 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = 'assets/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'portfolio-react-vite/dist/assets'
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
