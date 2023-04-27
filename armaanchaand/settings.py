1a2bb86fdd30dac59947e0047cacdb20c7184eb3
ArmaanChaand <armaan.chaand17@gmail.com>
2023-04-27 16:13:34 +0530

    Settings Requirements

--- armaanchaand/settings.py
+++ armaanchaand/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-0=7tvpfm*b#l=#m!a^4no
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == "1"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
